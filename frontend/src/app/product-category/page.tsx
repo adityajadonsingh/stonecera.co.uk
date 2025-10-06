fb9fafae695a11a7345e9fe958d64d64ec093bf3
Aditya Jadon <51057603+adityajadonsingh@users.noreply.github.com>
2025-10-06 16:51:36 +0530

    Update page.tsx

--- frontend/src/app/product-category/page.tsx
+++ frontend/src/app/product-category/page.tsx
@@ -10,20 +10,27 @@ export default async function ProductCategoryPage() {
       </h1>
       <div className="px-10">
         <div className="grid grid-cols-4 gap-6">
-          {
-            categories.map((category, i) => <Link key={i+"key"} href={`/product-category/${category.slug}`}>
-            <div className="card h-48 relative">
-                <Image
-                    src={process.env.NEXT_PUBLIC_MEDIA_URL+category.images[0]?.url || '/placeholder.png'}
-                    alt={category.images[0]?.alt|| category.name}
+          {categories.map((category, i) => {
+            console.log(process.env.NEXT_PUBLIC_MEDIA_URL+category.images[0]?.url);
+            return (
+              <Link key={i + "key"} href={`/product-category/${category.slug}`}>
+                <div className="card h-48 relative">
+                  <Image
+                    src={
+                      process.env.NEXT_PUBLIC_MEDIA_URL +
+                        category.images[0]?.url || "/placeholder.png"
+                    }
+                    alt={category.images[0]?.alt || category.name}
                     fill
                     className="object-cover rounded-lg+"
-                />
-            </div>
-            <h2 className="text-center mt-2 font-semibold">{category.name}</h2>
-          </Link>)
-          }
-
+                  />
+                </div>
+                <h2 className="text-center mt-2 font-semibold">
+                  {category.name}
+                </h2>
+              </Link>
+            );
+          })}
         </div>
       </div>
     </>
