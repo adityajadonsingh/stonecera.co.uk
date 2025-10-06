c8d328a6e4a9861911a00345e8888043d1838813
Aditya Jadon <51057603+adityajadonsingh@users.noreply.github.com>
2025-10-06 16:01:50 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['localhost', 'localhost:1337'],
+    domains: [
+      "localhost",
+      "localhost:1337",
+      "admin.stonecera.co.uk",
+      "stonecera.co.uk",
+    ],
   },
 };
 
