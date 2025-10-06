174467daa50057f540f15c20854e29312228e1df
Aditya Jadon <51057603+adityajadonsingh@users.noreply.github.com>
2025-10-06 16:41:45 +0530

    Update middlewares.js

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,8 +1,50 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+
+  // 🔒 Security settings: allow images/media from your domains
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'admin.stonecera.co.uk',
+            'stonecera.co.uk',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'admin.stonecera.co.uk',
+            'stonecera.co.uk',
+          ],
+        },
+      },
+    },
+  },
+
+  // 🌍 CORS configuration
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'https://stonecera.co.uk',
+        'https://www.stonecera.co.uk',
+        'https://admin.stonecera.co.uk',
+        'http://localhost:3000',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
+
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
